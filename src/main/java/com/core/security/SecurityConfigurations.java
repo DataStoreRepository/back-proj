1303381c6457e9849ecedde818003e794c85c7a3
Dutra-Wesley <wesley-dutra17@hotmail.com>
2024-11-28 21:42:31 -0300

    fix

--- src/main/java/com/core/security/SecurityConfigurations.java
+++ src/main/java/com/core/security/SecurityConfigurations.java
@@ -13,6 +13,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -60,18 +63,18 @@ public class SecurityConfigurations {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("https://front-proj-ku7s.onrender.com"); //
-        // Frontend URL
-        configuration.addAllowedMethod("*"); // Permitir todos os métodos HTTP
-        configuration.addAllowedHeader("*"); // Permitir todos os cabeçalhos
-        configuration.setAllowCredentials(true); // Permitir cookies e autenticação
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
+    // @Bean
+    // public CorsConfigurationSource corsConfigurationSource() {
+    //     CorsConfiguration configuration = new CorsConfiguration();
+    //     configuration.addAllowedOrigin("https://front-proj-ku7s.onrender.com"); //
+    //     // Frontend URL
+    //     configuration.addAllowedMethod("*"); // Permitir todos os métodos HTTP
+    //     configuration.addAllowedHeader("*"); // Permitir todos os cabeçalhos
+    //     configuration.setAllowCredentials(true); // Permitir cookies e autenticação
+    //     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    //     source.registerCorsConfiguration("/**", configuration);
+    //     return source;
+    // }
 
     // @Bean
     // public CorsConfigurationSource corsConfigurationSource() {
